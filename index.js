48bc1606928a50853fe6dc50bc68eed8a7cec833
Isaiah <zaymgmt707@gmail.com>
2024-07-15 14:00:41 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const currentUser = 'Grace Hopper!';
+// Variable to store the name of the current user
+const currentUser = 'Grace Hopper!'
+// Variable to store a welcome message for the current user
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// Variable to store an excited welcome message in all uppercase letters
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+// Variable to store a short greeting that includes only the first letter of the current user's name
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
